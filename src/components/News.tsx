f4c15cc098477f75943556e2bdbb19d2427bc4ae
Danny <142972494+DannyAkintunde@users.noreply.github.com>
2024-05-22 18:34:55 +0100

    Update News.tsx

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,22 +1,40 @@
-import Image from "next/image"
-import Link from "next/link"
-import { getNewsInfo } from "@/api"
-import { newsType } from "@/types"
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { getNewsInfo } from "@/api";
+import { newsType } from "@/types";
 
-const News = async () => {
-  const getNews = await getNewsInfo()
+const News = () => {
+  const [newsData, setNewsData] = useState([]);
 
-  const newsData:newsType[] = getNews.articles
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const news = await getNewsInfo();
+        setNewsData(news.articles);
+      } catch (error) {
+        console.error("Error fetching news:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <div className='w-[350px] bg-[rgb(40,46,58)] rounded-md px-2 md:px-6 py-2'>
-      <h1 className='text-xl text-teal-400 font-bold mb-4'>News - Top Headlines</h1>
+    <div className="w-[350px] bg-[rgb(40,46,58)] rounded-md px-2 md:px-6 py-2">
+      <h1 className="text-xl text-teal-400 font-bold mb-4">News - Top Headlines</h1>
       <div>
         {newsData.map((news) => (
-          <Link key={`${news.title}`} href={news.url} legacyBehavior>
+          <Link key={news.title} href={news.url} passHref>
             <a target="_blank">
               <div className="relative w-full h-[150px] mb-4 group">
-                <Image src={news?.urlToImage != null ? news?.urlToImage :  '/img/news-football.webp'} alt={news.title} fill className="object-cover rounded-md" />
+                <Image
+                  src={news?.urlToImage != null ? news?.urlToImage : '/img/news-football.webp'}
+                  alt={news.title}
+                  width={350}
+                  height={150}
+                  className="object-cover rounded-md"
+                />
                 <div className="absolute bottom-0 left-0 w-full p-2 z-10 bg-gradient-to-t from-zinc-900 to-transparent">
                   <p className="font-semibold text-lg group-hover:text-teal-400">{news.title}</p>
                 </div>
@@ -26,7 +44,7 @@ const News = async () => {
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default News
+export default News;
